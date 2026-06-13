55be5349fadb4c87d149343493035697c817ad0b
Miguel Calle <miguel_calle82231@elpoli.edu.co>
2026-06-13 08:19:12 -0500

    test

--- pyapi/src/main/java/api/pyapi/Security/SecurityConfig.java
+++ pyapi/src/main/java/api/pyapi/Security/SecurityConfig.java
@@ -32,7 +32,10 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated())
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin()))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
